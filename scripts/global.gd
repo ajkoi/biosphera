028f1d4608696d934ba430d4bf69bb41f0c31e62
YIOU Félix <felix.yiou.alias.yeou@gmail.com>
2026-05-09 15:04:31 +0200

    global script

---
+++ scripts/global.gd
@@ -0,0 +1,23 @@
+extends Node
+var possessed_card = {
+	"card1" = true,
+	"card2" = true,
+	"card3" = false,
+	"card4" = false,
+	"card5" = false,
+	"card6" = false,
+	"card7" = false,
+	"card8" = false,
+	"card9" = false,
+	"card10" = false,
+}
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
