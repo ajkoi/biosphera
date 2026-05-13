3dae7bbcd53b860aa3ac720bf070e654e75c0af5
YIOU Félix <felix.yiou.alias.yeou@gmail.com>
2026-05-14 00:31:22 +0200

    ouf rien de cassé

--- scripts/change_scene.gd
+++ scripts/change_scene.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+# ce script est pour changer de scene mais il ne sert pas a grand chose dans le jeu
+
 const next_scene = preload("res://scenes/menu_arbre/main_scene_arbre.tscn")
 func _on_body_entered(body: Node2D) -> void:
 	if "player" in body.get_groups():		
