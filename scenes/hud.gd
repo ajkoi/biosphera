9fe9a1300c87f89ab81715c1add2be15ad970dac
YIOU Félix <felix.yiou.alias.yeou@gmail.com>
2026-05-20 17:03:12 +0200

    hud

--- scenes/hud.gd
+++ scenes/hud.gd
@@ -4,18 +4,20 @@ extends CanvasLayer
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Label.visible = false
+	$bulle.visible = false
 	for place in get_tree().get_nodes_in_group("capture_places"):
 		place.capture.connect(on_capture)
 
 
 
 func on_capture(card):
-	print(card)
 	$Timer.start()
 	$Label.visible = true
-	$Label.text = "Tu as eu la carte" + card
+	$bulle.visible = true
+	$Label.text = "Tu as eu la carte de" + card
 	global.possessed_card[card] = true
 
 
 func _on_timer_timeout() -> void:
 	$Label.visible = false
+	$bulle.visible = false
