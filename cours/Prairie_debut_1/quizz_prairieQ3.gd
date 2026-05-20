b44b508ac2fe8bf4e0f07bef96f84c9a7649d8ce
YIOU Félix <felix.yiou.alias.yeou@gmail.com>
2026-05-20 14:04:18 +0200

    les cours de la prairie

---
+++ cours/Prairie_debut_1/quizz_prairieQ3.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+signal fin_text
+
+func card_on(snap : Node2D):
+	var pos = snap.position
+	for i in get_tree().get_nodes_in_group("cards"):
+		if pos == i.position:
+			return true
+	return false
+
+
+
+
+func _on_button_pressed() -> void:
+	if card_on($snap_area) && card_on($snap_area2) && $snap_area3.position == $card3.position:
+		print("c'est bon :DD")
+		fin_text.emit()
+	elif card_on($snap_area) && card_on($snap_area3) && $snap_area3.position == $card2.position:
+		print("c'est bon :DD")
+		fin_text.emit()
+	else:
+		print("essaie encore")
